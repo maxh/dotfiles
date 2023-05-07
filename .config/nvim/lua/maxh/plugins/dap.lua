beb34426a8b0b020d64f8fdf26f1b93d4d0b6cca
Max Heinritz <maxheinritz@gmail.com>
2023-05-07 17:55:45 -0500

    fugitive and dap tweaks

--- .config/nvim/lua/maxh/plugins/dap.lua
+++ .config/nvim/lua/maxh/plugins/dap.lua
@@ -1,162 +1,162 @@
 return {
-  "mfussenegger/nvim-dap",
-  dependencies = {
-    "mxsdev/nvim-dap-vscode-js",
-    "rcarriga/nvim-dap-ui",
-  },
-  config = function()
-    vim.fn.sign_define("DapBreakpoint", { text = "🔴", texthl = "", linehl = "", numhl = "" })
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"mxsdev/nvim-dap-vscode-js",
+		"rcarriga/nvim-dap-ui",
+	},
+	config = function()
+		vim.fn.sign_define("DapBreakpoint", { text = "🔴", texthl = "", linehl = "", numhl = "" })
 
-    vim.api.nvim_set_keymap(
-      "n",
-      "<Leader>dj",
-      ':lua require("dap").run_last()<CR>',
-      { noremap = true, silent = true }
-    )
+		vim.api.nvim_set_keymap(
+			"n",
+			"<Leader>dj",
+			':lua require("dap").run_last()<CR>',
+			{ noremap = true, silent = true }
+		)
 
-    vim.api.nvim_set_keymap(
-      "n",
-      "<Leader>db",
-      ':lua require("dap").toggle_breakpoint()<CR>',
-      { noremap = true, silent = true }
-    )
-    vim.api.nvim_set_keymap(
-      "n",
-      "<Leader>di",
-      ':lua require("dap").step_into()<CR>',
-      { noremap = true, silent = true }
-    )
-    vim.api.nvim_set_keymap(
-      "n",
-      "<Leader>do",
-      ':lua require("dap").step_over()<CR>',
-      { noremap = true, silent = true }
-    )
-    vim.api.nvim_set_keymap(
-      "n",
-      "<Leader>du",
-      ':lua require("dap").step_out()<CR>',
-      { noremap = true, silent = true }
-    )
-    vim.api.nvim_set_keymap(
-      "n",
-      "<Leader>dc",
-      ':lua require("dap").continue()<CR>',
-      { noremap = true, silent = true }
-    )
-    vim.api.nvim_set_keymap(
-      "n",
-      "<Leader>dr",
-      ':lua require("dap").restart()<CR>',
-      { noremap = true, silent = true }
-    )
-    vim.api.nvim_set_keymap(
-      "n",
-      "<Leader>dx",
-      ':lua require("dap").disconnect()<CR>',
-      { noremap = true, silent = true }
-    )
-    vim.api.nvim_set_keymap(
-      "n",
-      "<Leader>dq",
-      ':lua require("dapui").close()<CR>',
-      { noremap = true, silent = true }
-    )
+		vim.api.nvim_set_keymap(
+			"n",
+			"<Leader>db",
+			':lua require("dap").toggle_breakpoint()<CR>',
+			{ noremap = true, silent = true }
+		)
+		vim.api.nvim_set_keymap(
+			"n",
+			"<Leader>di",
+			':lua require("dap").step_into()<CR>',
+			{ noremap = true, silent = true }
+		)
+		vim.api.nvim_set_keymap(
+			"n",
+			"<Leader>do",
+			':lua require("dap").step_over()<CR>',
+			{ noremap = true, silent = true }
+		)
+		vim.api.nvim_set_keymap(
+			"n",
+			"<Leader>du",
+			':lua require("dap").step_out()<CR>',
+			{ noremap = true, silent = true }
+		)
+		vim.api.nvim_set_keymap(
+			"n",
+			"<Leader>dc",
+			':lua require("dap").continue()<CR>',
+			{ noremap = true, silent = true }
+		)
+		vim.api.nvim_set_keymap(
+			"n",
+			"<Leader>dr",
+			':lua require("dap").restart()<CR>',
+			{ noremap = true, silent = true }
+		)
+		vim.api.nvim_set_keymap(
+			"n",
+			"<Leader>dx",
+			':lua require("dap").disconnect()<CR>',
+			{ noremap = true, silent = true }
+		)
+		vim.api.nvim_set_keymap(
+			"n",
+			"<Leader>dt",
+			':lua require("dapui").toggle()<CR>',
+			{ noremap = true, silent = true }
+		)
 
-    require("dap-vscode-js").setup({
-      node_path = "node",
-      debugger_path = os.getenv("HOME") .. "/tools/vscode-js-debug",
-      adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" },
-    })
-    local dap = require("dap")
-    dap.adapters.lldb = {
-      type = "executable",
-      command = "/opt/homebrew/opt/llvm/bin/lldb-vscode",
-      name = "lldb",
-    }
-    for _, language in ipairs({ "typescript", "javascript" }) do
-      require("dap").configurations[language] = {
-        {
-          type = "pwa-node",
-          request = "launch",
-          name = "Launch file",
-          program = "${file}",
-          cwd = "${workspaceFolder}",
-        },
-        {
-          type = "pwa-node",
-          request = "attach",
-          name = "Attach",
-          processId = require("dap.utils").pick_process,
-          cwd = "${workspaceFolder}",
-        },
-        {
-          type = "pwa-node",
-          request = "launch",
-          name = "Launch test current file (pwa-node with jest)",
-          cwd = vim.fn.getcwd(),
-          runtimeArgs = { "${workspaceFolder}/node_modules/.bin/jest" },
-          runtimeExecutable = "node",
-          args = { "${file}", "--coverage", "false" },
-          rootPath = "${workspaceFolder}",
-          sourceMaps = true,
-          console = "integratedTerminal",
-          internalConsoleOptions = "neverOpen",
-          skipFiles = { "<node_internals>/**", "node_modules/**" },
-        },
-      }
-    end
+		require("dap-vscode-js").setup({
+			node_path = "node",
+			debugger_path = os.getenv("HOME") .. "/tools/vscode-js-debug",
+			adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" },
+		})
+		local dap = require("dap")
+		dap.adapters.lldb = {
+			type = "executable",
+			command = "/opt/homebrew/opt/llvm/bin/lldb-vscode",
+			name = "lldb",
+		}
+		for _, language in ipairs({ "typescript", "javascript" }) do
+			require("dap").configurations[language] = {
+				{
+					type = "pwa-node",
+					request = "launch",
+					name = "Launch file",
+					program = "${file}",
+					cwd = "${workspaceFolder}",
+				},
+				{
+					type = "pwa-node",
+					request = "attach",
+					name = "Attach",
+					processId = require("dap.utils").pick_process,
+					cwd = "${workspaceFolder}",
+				},
+				{
+					type = "pwa-node",
+					request = "launch",
+					name = "Launch test current file (pwa-node with jest)",
+					cwd = vim.fn.getcwd(),
+					runtimeArgs = { "${workspaceFolder}/node_modules/.bin/jest" },
+					runtimeExecutable = "node",
+					args = { "${file}", "--coverage", "false" },
+					rootPath = "${workspaceFolder}",
+					sourceMaps = true,
+					console = "integratedTerminal",
+					internalConsoleOptions = "neverOpen",
+					skipFiles = { "<node_internals>/**", "node_modules/**" },
+				},
+			}
+		end
 
-    local dap = require("dap")
-    dap.configurations.cpp = {
-      {
-        name = "Launch",
-        type = "lldb",
-        request = "launch",
-        program = function()
-          return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-        end,
-        cwd = "${workspaceFolder}",
-        stopOnEntry = false,
-        args = {},
+		local dap = require("dap")
+		dap.configurations.cpp = {
+			{
+				name = "Launch",
+				type = "lldb",
+				request = "launch",
+				program = function()
+					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+				end,
+				cwd = "${workspaceFolder}",
+				stopOnEntry = false,
+				args = {},
 
-        -- 💀
-        -- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
-        --
-        --    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
-        --
-        -- Otherwise you might get the following error:
-        --
-        --    Error on launch: Failed to attach to the target process
-        --
-        -- But you should be aware of the implications:
-        -- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
-        -- runInTerminal = false,
-      },
-    }
-    dap.configurations.c = dap.configurations.cpp
+				-- 💀
+				-- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
+				--
+				--    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+				--
+				-- Otherwise you might get the following error:
+				--
+				--    Error on launch: Failed to attach to the target process
+				--
+				-- But you should be aware of the implications:
+				-- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
+				-- runInTerminal = false,
+			},
+		}
+		dap.configurations.c = dap.configurations.cpp
 
-    require("dapui").setup({
-      layouts = {
-        {
-          elements = {
-            {
-              id = "repl",
-              size = 0.5,
-            },
-            {
-              id = "console",
-              size = 0.5,
-            },
-          },
-          position = "bottom",
-          size = 10,
-        },
-      },
-    })
-    local dapui = require("dapui")
-    dap.listeners.after.event_initialized["dapui_config"] = function()
-      dapui.open()
-    end
-  end,
+		require("dapui").setup({
+			layouts = {
+				{
+					elements = {
+						{
+							id = "repl",
+							size = 0.5,
+						},
+						{
+							id = "console",
+							size = 0.5,
+						},
+					},
+					position = "bottom",
+					size = 10,
+				},
+			},
+		})
+		local dapui = require("dapui")
+		dap.listeners.after.event_initialized["dapui_config"] = function()
+			dapui.open()
+		end
+	end,
 }
--- .config/nvim/lua/maxh/plugins/misc.lua
+++ .config/nvim/lua/maxh/plugins/misc.lua
@@ -1,36 +1,37 @@
 return {
-  {
-    "dmmulroy/tsc.nvim",
-    config = true,
-  },
-  {
-    "folke/neodev.nvim",
-    dependencies = {
-      "nvim-dap-ui",
-    },
-    config = function()
-      require("neodev").setup({
-        library = { plugins = { "nvim-dap-ui" }, types = true },
-      })
-    end,
-  },
-  { "kana/vim-textobj-entire", dependencies = { "kana/vim-textobj-user" } },
-  "kkharji/sqlite.lua",
-  "RRethy/vim-illuminate",
-  { "stevearc/dressing.nvim",  config = true },
-  "tpope/vim-abolish",
-  "tpope/vim-commentary",
-  {
-    "windwp/nvim-autopairs",
-    config = true,
-  },
-  {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      require("gitsigns").setup({ current_line_blame = true })
-    end,
-  },
-  {
-    "nvim-lualine/lualine.nvim",
-  },
+	{
+		"dmmulroy/tsc.nvim",
+		config = true,
+	},
+	{
+		"folke/neodev.nvim",
+		dependencies = {
+			"nvim-dap-ui",
+		},
+		config = function()
+			require("neodev").setup({
+				library = { plugins = { "nvim-dap-ui" }, types = true },
+			})
+		end,
+	},
+	{ "kana/vim-textobj-entire", dependencies = { "kana/vim-textobj-user" } },
+	"kkharji/sqlite.lua",
+	"RRethy/vim-illuminate",
+	{ "stevearc/dressing.nvim", config = true },
+	"tpope/vim-abolish",
+	"tpope/vim-commentary",
+	"tpope/vim-fugitive",
+	{
+		"windwp/nvim-autopairs",
+		config = true,
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup({ current_line_blame = true })
+		end,
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+	},
 }
