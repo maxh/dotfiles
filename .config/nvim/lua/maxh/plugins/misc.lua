7efbc679a99a0ad472ba1ae5022195bb883ac791
Max Heinritz <maxheinritz@gmail.com>
2023-05-13 08:30:24 -0500

    clean up plugins

--- .config/nvim/lua/maxh/plugins/misc.lua
+++ .config/nvim/lua/maxh/plugins/misc.lua
@@ -8,53 +8,13 @@ return {
 		dependencies = {
 			"nvim-dap-ui",
 		},
-		config = function()
-			require("neodev").setup({
-				library = { plugins = { "nvim-dap-ui" }, types = true },
-			})
-		end,
-	},
-	{
-		"junegunn/goyo.vim",
-		config = function()
-			vim.keymap.set("n", "<Leader>zg", "<Cmd>:Goyo<CR>", { desc = "Goyo 'zen' mode" })
-		end,
-	},
-	{
-		"junegunn/limelight.vim",
-		dependencies = { "junegunn/goyo.vim" },
-		config = function()
-			vim.g.limelight_paragraph_span = 0
-			vim.api.nvim_create_autocmd("User", { pattern = "GoyoEnter", command = "Limelight" })
-			vim.api.nvim_create_autocmd("User", { pattern = "GoyoLeave", command = "Limelight!" })
-		end,
+		opts = {
+			library = { plugins = { "nvim-dap-ui" }, types = true },
+		},
 	},
 	{ "kana/vim-textobj-entire", dependencies = { "kana/vim-textobj-user" } },
 	"kkharji/sqlite.lua",
 	"RRethy/vim-illuminate",
-	{
-		"zbirenbaum/copilot.lua",
-		cmd = "Copilot",
-		event = "InsertEnter",
-		opts = {
-			suggestion = {
-				enabled = true,
-				auto_trigger = true,
-				debounce = 75,
-				keymap = {
-					accept = "<Tab>",
-					accept_word = false,
-					accept_line = false,
-					next = "<C-j>",
-					prev = "<C-k>",
-					dismiss = "<C-e>",
-				},
-			},
-			panel = {
-				enabled = false,
-			},
-		},
-	},
 	{
 		"stevearc/dressing.nvim",
 		opts = {
@@ -67,7 +27,6 @@ return {
 				disabled = true,
 			},
 		},
-		config = true,
 	},
 	"tpope/vim-abolish",
 	"tpope/vim-commentary",
@@ -79,8 +38,7 @@ return {
 	},
 	{
 		"lewis6991/gitsigns.nvim",
-		config = function()
-			require("gitsigns").setup({ current_line_blame = true })
-		end,
+		opts = { current_line_blame = true },
+		config = true,
 	},
 }
