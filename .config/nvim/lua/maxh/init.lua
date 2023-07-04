e00b4db347b0b8fdc459d763d4328fa966fec559
Max Heinritz <maxheinritz@gmail.com>
2023-07-04 12:36:46 -0500

    add todos

--- .config/nvim/lua/maxh/init.lua
+++ .config/nvim/lua/maxh/init.lua
@@ -3,18 +3,18 @@ set.background = "dark"
 set.colorcolumn = "80"
 set.expandtab = true -- Use spaces instead of tabs
 set.linebreak = true
-set.number = true    -- Line numbers
+set.number = true -- Line numbers
 set.relativenumber = true
-set.shiftwidth = 2   -- Size of an indent
+set.shiftwidth = 2 -- Size of an indent
 set.softtabstop = 2
-set.tabstop = 2      -- Number of spaces tabs count for
+set.tabstop = 2 -- Number of spaces tabs count for
 
 -- No tildes for empty lines.
 -- https://github.com/neovim/neovim/issues/2067
 vim.opt.fillchars = { eob = " " }
 
 if vim.fn.has("termguicolors") == 1 then
-  vim.o.termguicolors = true
+	vim.o.termguicolors = true
 end
 
 require("maxh.isolated_history")
@@ -22,20 +22,22 @@ require("maxh.remap")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("maxh/plugins", {})
 
 -- TO CONFIG
+-- fugitive stage all
+-- fugitive push all
 -- https://github.com/tpope/vim-dadbod
 -- https://github.com/ruifm/gitlinker.nvim
 -- https://github.com/kylechui/nvim-surround
