1332be303615e552d85c76d2654037b3d2aeeafb
Max Heinritz <max@loop.com>
2023-11-21 11:14:50 -0600

    init

--- .config/nvim/lua/maxh/plugins/lint.lua
+++ .config/nvim/lua/maxh/plugins/lint.lua
@@ -24,6 +24,33 @@ return {
   },
   config = function(_, opts)
     local lint = require("lint")
+    require('lint').linters.your_linter_name = {
+      cmd = 'node /Users/max/loop/prisma-lint/dist/cli.js',
+      stdin = false,
+      args = { "--output-format", "json" },
+      append_fname = true,
+      stream = 'stdout',
+      ignore_exitcode = false,
+      parser = function(output, bufnr)
+        local decoded = vim.json.decode(output)
+        local diagnostics = {}
+        local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, true)
+        local content = table.concat(lines, '\n')
+        for _, match in pairs(decoded.matches or {}) do
+          local byteidx = vim.fn.byteidx(content, match.offset)
+          local line = vim.fn.byte2line(byteidx)
+          local col = byteidx - vim.fn.line2byte(line)
+          table.insert(diagnostics, {
+            lnum = line - 1,
+            end_lnum = line - 1,
+            col = col + 1,
+            end_col = col + 1,
+            message = match.message,
+          })
+        end
+        return diagnostics
+      end,
+    }
     lint.linters_by_ft = opts.linters_by_ft
     for k, v in pairs(opts.linters) do
       lint.linters[k] = v
