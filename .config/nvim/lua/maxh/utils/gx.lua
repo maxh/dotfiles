21b98b14c7a0f73a5cc4db15e68c135b42cd01a1
Max Heinritz <max@loop.com>
2023-11-21 22:42:12 -0600

    clean

--- .config/nvim/lua/maxh/utils/gx.lua
+++ .config/nvim/lua/maxh/utils/gx.lua
@@ -2,25 +2,25 @@ local M = {}
 
 -- Define a function to open URLs in the default web browser
 local function open_url(url)
-  os.execute("open " .. url)
+	os.execute("open " .. url)
 end
 
 -- Define a function to extract the URL from Markdown syntax
 local function extract_url(line)
-  local url = line:match("<(https?://[^>]+)>") or line:match("['\"](https?://[^'\"]+)['\"]")
-  return url
+	local url = line:match("<(https?://[^>]+)>") or line:match("(https?://[^%s]+)")
+	return url
 end
 
 -- Define a function to handle the gx mapping
 function M.open_url_under_cursor()
-  local line = vim.fn.getline(".")
-  local url = extract_url(line)
+	local line = vim.fn.getline(".")
+	local url = extract_url(line)
 
-  if url then
-    open_url(url)
-  else
-    print("No URL found under cursor")
-  end
+	if url then
+		open_url(url)
+	else
+		print("No URL found under cursor")
+	end
 end
 
 return M
